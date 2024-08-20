fd2b3f0434925f99533023d1eff4fe2e4771fb88
Africar-Lee <africar@126.com>
2024-08-20 18:21:14 +0800

    增加终端显示控制功能

--- main.cpp
+++ main.cpp
@@ -1,15 +1,37 @@
 # include <iostream>
-# include <unistd.h>
+# include <thread>
+# include <chrono>
+# include "terminal.h"
+
+using namespace std::chrono_literals;
 
 int
 main (void)
 {
-    std::cout << "hello, world\n";
+    // std::cout << "\033[5;10H"
+    //           << "\033[38;5;214m"
+    //           << "HelloWorld"
+    //           << "\033[12;1H";
 
-    int i = 0;
-    while (i < 10) {
-        i++;
+    // tc::cursor_move_to(5, 10);
+    // tc::set_fore_color(214);
+    // std::cout << "HelloWorld";
+    // tc::cursor_move_to(10, 1);
+
+    tc::hide_cursor();
+
+    int i = 1;
+    while (true) {
+        std::this_thread::sleep_for(500ms);
+
+        tc::clean_screen();
+
+        tc::cursor_move_to(i++, 10);
+        tc::set_back_color(15);
+        std::cout << "  " << std::flush;
+        tc::reset_color();
+        tc::cursor_move_to(10, 1);
     }
 
     return 0;
-} 
+}
---
+++ terminal.h
@@ -0,0 +1,10 @@
+namespace tc { // terminal control
+
+    void cursor_move_to(int row, int col);
+    void set_fore_color(int color_id);
+    void set_back_color(int color_id);
+    void clean_screen();
+    void reset_color();
+    void hide_cursor();
+    void show_cursor();
+}
